62a23d4b97d5acddf66c9784211a9be28eec46a2
rodrigo <rodrigombarreda@gmail.com>
2020-11-11 20:19:46 -0300

    11/11/2020

---
+++ app/src/main/java/com/example/futbolonline/fragments/cambioContraseniaViewModel.kt
@@ -0,0 +1,71 @@
+package com.example.futbolonline.fragments
+
+import androidx.lifecycle.ViewModel
+import com.example.futbolonline.entidades.Usuario
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
+
+class cambioContraseniaViewModel: ViewModel(){
+    val db = Firebase.firestore
+    suspend fun esLaMismaContrasenia(
+        contrasenia: String,
+        mail: String?
+    ): Boolean {
+        var ContraseniaCorrectas: Boolean = false
+        if (mail != null) {
+            if(!contrasenia.isBlank() && !mail.isBlank()){
+                val questionRef = db.collection("usuarios").document(mail)
+                val query = questionRef
+                try {
+                    val data = query
+                        .get().await()
+                    if(data!=null){
+                        val usuario = data.toObject<Usuario>()
+                        if (usuario != null){
+                            if(usuario.contrasenia==contrasenia){
+                                ContraseniaCorrectas=true
+                            }
+                        }
+                    }
+                }catch (e: Exception){
+
+                }
+            }
+        }
+
+
+        return ContraseniaCorrectas
+    }
+    suspend fun cambiarContrasenia(contraseniaNuevaUno: String,contraseniaNuevaDos:String,mail: String?):Boolean{
+        var contraseniaCambiada: Boolean = false
+        if(mail!=null){
+            if(!contraseniaNuevaUno.isBlank() && !contraseniaNuevaDos.isBlank()){
+                val questionRef = db.collection("usuarios").document(mail)
+                val query = questionRef
+                try {
+                    if (contraseniaNuevaDos==contraseniaNuevaUno){
+                        val data = query
+                            .get()
+                            .await()
+                        if(data!=null){
+                            val usuarioBd = data.toObject<Usuario>()
+                            if(usuarioBd!=null){
+                                val usuario:Usuario=Usuario(mail,usuarioBd.nombre,usuarioBd.genero,usuarioBd.edad,contraseniaNuevaUno,usuarioBd.calificacion)
+                                db.collection("usuarios").document(mail).set(usuario)
+                                contraseniaCambiada=true
+                            }
+                        }
+
+                    }
+
+                }catch (e: Exception){
+
+                }
+            }
+        }
+        return contraseniaCambiada
+    }
+
+}
--- app/src/main/java/com/example/futbolonline/fragments/cambiocontrasenia.kt
+++ app/src/main/java/com/example/futbolonline/fragments/cambiocontrasenia.kt
@@ -1,22 +1,103 @@
 package com.example.futbolonline.fragments
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
 import com.example.futbolonline.R
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 
 class cambiocontrasenia : Fragment() {
 
+
+    companion object {
+        fun newInstance() = cambiocontrasenia()
+    }
+
+    val USUARIO_PREFERENCES: String = "usuarioPreferences"
+    private lateinit var CambioContraseniaViewModel: cambioContraseniaViewModel
+
+    lateinit var passActual: EditText
+    lateinit var passNewUno: EditText
+    lateinit var passNewDos: EditText
+    lateinit var confirmar: Button
+    lateinit var volver: Button
     lateinit var v: View
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         v = inflater.inflate(R.layout.fragment_cambiocontrasenia, container, false)
+        passActual = v.findViewById(R.id.passActual)
+        passNewUno = v.findViewById(R.id.passNewUno)
+        passNewDos = v.findViewById(R.id.passNewDos)
+        confirmar = v.findViewById(R.id.buttonConfirmar)
+        volver = v.findViewById(R.id.buttonVolver)
+
         return v
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        CambioContraseniaViewModel =
+            ViewModelProvider(this).get(cambioContraseniaViewModel::class.java)
+
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val parentJob = Job()
+        val scope = CoroutineScope(Dispatchers.Default + parentJob)
+
+
+        confirmar.setOnClickListener {
+            scope.launch {
+                val sharedPref: SharedPreferences = requireContext().getSharedPreferences(
+                    USUARIO_PREFERENCES,
+                    Context.MODE_PRIVATE
+                )
+                var mail = sharedPref.getString(
+                    "EMAIL_USUARIO",
+                    "default"
+                )
+                var esMismaContrasenia = CambioContraseniaViewModel.esLaMismaContrasenia(
+                    passActual.text.toString(),
+                    mail
+                )
+
+                if (esMismaContrasenia) {
+                    var seCambio = CambioContraseniaViewModel.cambiarContrasenia(
+                        passNewUno.text.toString(),
+                        passNewDos.text.toString(),
+                        mail
+                    )
+                    if (seCambio) {
+                        Snackbar.make(
+                            v,
+                            "contraseña cambiada correctamente",
+                            Snackbar.LENGTH_SHORT
+                        ).show()
+
+
+                    }
+
+
+                }
+            }
+        }
+    }
 }
